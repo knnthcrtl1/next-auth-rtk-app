c16721a9b4e44e922a16eee7cb34c22ce623f806
DESKTOP-0MNRU9B\Kenneth <knnthcrtl.kc@gmail.com>
2023-06-03 20:08:38 +0800

    setup counter slice rtk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
+import React from "react";
+import "./index.css";
+import { store } from "./redux/confgureStore";
+import { Provider } from "react-redux";
 
 export default function Home() {
   return (
-    <div className="container">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ipsum,
-      dolor recusandae quas repellendus dicta suscipit? Facere, amet, voluptate,
-      ea eligendi similique at obcaecati optio est officiis ullam sit
-      blanditiis?
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex bg-white p-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae in nam
+        architecto aperiam, nobis velit quod dolore atque illo id neque nihil
+        quasi beatae? Voluptatem non alias nemo repellat.
+      </div>
+    </main>
   );
 }
